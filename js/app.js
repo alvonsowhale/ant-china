c500a9caf23cfd67a4aa2b1f3a6a35d182193de5
alvonsowhale <154766786+alvonsowhale@users.noreply.github.com>
2023-12-25 07:12:36 +0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function followToGPT() {
+  window.open('https://chat.openai.com/g/g-Tnp7u3mHl-antai', '_blank');
+}
+
+
+function playAntSound() {
+  var audio = new Audio('../img/Ant_Sound_Yeah_Lol_We_Take_It_From_Roflz_Video.mp3');
+  audio.play();
+}
